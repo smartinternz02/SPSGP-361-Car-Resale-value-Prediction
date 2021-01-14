ce4126b0875f5206b5ecf87de3d9a486ab6148a0
sani1001 <77077408+sani1001@users.noreply.github.com>
2021-01-14 00:37:10 -0800

    import libraries and read read the dataset using ju

---
+++ import liabries and read the dataset.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+import matplotlib as plt
+from sklearn.preprocessing import LabelEncoder
+import pickle
+
+
+# In[15]:
+
+
+df = pd.read_csv("F:/DATA SCIENCE PROJECT/Data/autos.csv", header=0, sep=',', encoding='Latin1',)
+
+
+# In[16]:
+
+
+df.head
+
+
+# In[ ]:
+
+
+
+
